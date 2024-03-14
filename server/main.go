7bbbf699d9a7f5754ec18bb12c1580dae1d6719d
MBI <59748306+MBI-88@users.noreply.github.com>
2024-03-14 07:22:03 -0300

    modules added

--- server/main.go
+++ server/main.go
@@ -1,7 +1,33 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	
+
+	"github.com/MBI-88/fiber-grpc-crud/server/config"
+	"github.com/MBI-88/fiber-grpc-crud/server/models"
+	
+)
+
+func server() {
+	flag.Parse()
+	arg := flag.Arg(0)
+	env := config.Config{}.GetEnvVar() // cargar variables de entorno
+	
+	switch arg {
+	case "start":
+		models.DialDb(env.Dsn, "./logs/error.log") // conectar con la base de datos (sqlite)
+
+	case "migrate":
+		models.Migrate(env.Dsn)
+
+	default:
+		fmt.Println("[-] No action taken")
+	}
+
+}
 
 func main() {
-
-	
+	server()
 }
--- server/models/sqlite.go
+++ server/models/sqlite.go
@@ -1,2 +1,50 @@
 package models
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var (
+	DB *gorm.DB
+	fileLog *os.File
+	loggerError = log.New(fileLog, "Database Error =>", log.Ldate)
+)
+
+func setConnection(dsn string) *gorm.DB {
+	db, err := gorm.Open(sqlite.Open(dsn),&gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+		Logger: logger.Default.LogMode(logger.Error),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func createLogs(path string) {
+	fileLog, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+	loggerError.SetOutput(fileLog)
+}
+
+func Migrate(url string) {
+	DB = setConnection(url)
+	if err := DB.AutoMigrate(&User{}); err != nil {
+		panic(err)
+	}
+	fmt.Println("[+] Migration done!")
+}
+
+func DialDb(url, logpath string) {
+	DB = setConnection(url)
+	go createLogs(logpath)
+}
